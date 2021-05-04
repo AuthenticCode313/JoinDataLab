01c32af1006c7cc9b773851eec40bf2028cdbff1
AuthenticCode313 <80369849+AuthenticCode313@users.noreply.github.com>
2021-05-04 04:54:20 -0700

    First commit

---
+++ Order.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JoiningDataOOPLab
+{
+    class Order
+    {
+        //fields
+        private string _customerName;
+        private string _item;
+        private decimal _price;
+        private int _quantity;
+        //properties
+        public string CustomerName
+        {
+            get { return this._customerName; }
+            set { this._customerName = value; }
+        }
+        public string Item
+        {
+            get { return this._item; }
+            set { this._item = value; }
+        }
+        public decimal Price
+        {
+            get { return this._price; }
+            set { this._price = value; }
+        }
+        public int Quantity
+        {
+            get { return this._quantity; }
+            set { this._quantity = value; }
+        }
+        //constructor
+        public Order(string customerName, string item, decimal price, int quantity)
+        {
+            this._customerName = customerName;
+            this._item = item;
+            this._price = price;
+            this._quantity = quantity;
+        }
+
+        public override string ToString()
+        {
+            return $"{this._item}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JoiningDataOOPLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Order> orders = new List<Order>
+            {
+                    new Order ("Acme Hardware", "Mouse", 25, 3),
+                    new Order ("Acme Hardware", "Keyboard", 45, 2),
+                    new Order ("Falls Realty", "Macbook", 800, 2),
+                    new Order ("Julie's Morning Diner", "iPad", 525, 1),
+                    new Order ("Julie's Morning Diner", "Credit Card Reader", 45, 1)
+
+            };
+
+            Exercise1(orders);
+            Exercise2(orders);
+
+        }
+
+        public static void Exercise1(List<Order> orders)
+        {
+            var FirstOrders = orders.GroupBy(p => p.CustomerName).Select(p => p.First());
+           
+            foreach (Order orderss in FirstOrders)
+            {
+                Console.WriteLine(orderss.CustomerName);
+                Console.WriteLine(string.Format("\t{0, -20} {1, -20} {2, 0}{3,10}", "Item", "Price", "Quantity", "Total"));
+                for (int i = 0; i < orders.Count; i++)
+                {
+                    if (orderss.CustomerName == orders[i].CustomerName)
+                    {
+                        Console.WriteLine("\t{0, -21} {1, -20} {2, 0}{3,15}", orders[i].Item, orders[i].Price, orders[i].Quantity, orders[i].Price * orders[i].Quantity);
+                    }
+                }
+                
+            }
+        }
+            public static void Exercise2(List<Order> orders)
+            {
+                var distinctOrders = orders.GroupBy(o => o.CustomerName).Select(o => o.First());
+                //Console.WriteLine(string.Join(" ",distinctOrders));
+                decimal total = 0;
+                foreach (Order ordered in distinctOrders)
+                {
+                    Console.WriteLine(ordered.CustomerName);
+                    Console.WriteLine(string.Format("\t{0, -20} {1, -20} {2, 0}{3,10}", "Item", "Price", "Quantity", "Total"));
+                    for (int i = 0; i < orders.Count; i++)
+                    {
+                        if (ordered.CustomerName == orders[i].CustomerName)
+                        {
+                            Console.WriteLine("\t{0, -21} {1, -20} {2, 0}{3,15}", orders[i].Item, orders[i].Price, orders[i].Quantity, orders[i].Price * orders[i].Quantity);
+                            total += orders[i].Price * orders[i].Quantity;
+                        }
+                    }
+                    Console.WriteLine("\t{0,-20}{1,38}", "Total", total);
+                    Console.WriteLine();
+                    total = 0;
+                }
+            }
+
+
+
+        
+        
+    }
+}
